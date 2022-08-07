4bdf4565e204dd3fe303eb1767017ec17dedeb55
f2lpOliveira <devfullstack@disponivel.online>
2022-08-06 23:40:31 -0300

    Início do código para execução da aplicação.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
-console.log("Hello World!");
+/*
+* Calculadora IMC
+* @author linkedin.com/in/f2lp-oliveira
+*/
+
+let peso = document.querySelector('#input-peso');
+let altura = document.querySelector('#input-altura');
+let resultadoImc = document.querySelector('#resultado-imc');
+
+function inicio(){
+  let buttonCalculeImc = document.querySelector('#calcule-imc');
+  buttonCalculeImc.addEventListener('click', handleButtonClick);
+}
+function calculaImc(){
+  return peso / altura ** 2;
+}
+function handleButtonClick(){
+  peso = parseFloat(peso.value.replace(",","."));
+  altura = parseFloat(altura.value.replace(",","."));
+  imc = calculaImc();
+  resultadoImc.textContent = imc.toFixed(2).replace(".",",");
+  statusImc();
+}
+
+function statusImc(){
+  let statusImc = document.querySelector('#status-imc');
+
+  if(imc == 16 || imc <= 16.9){
+    return statusImc.textContent = "Muito abaixo do peso"
+  }else if(imc == 17 || imc <= 18.4){
+    return statusImc.textContent = "Abaixo do peso"
+  }else if(imc == 18.5 || imc <= 24.9){
+    return statusImc.textContent = "Peso normal"
+  }else if(imc == 25 || imc <= 29.9){
+    return statusImc.textContent = "Acima do peso"
+  }else if(imc == 30 || imc <= 34.9){
+    return statusImc.textContent = "Obesidade grau I"
+  }else if(imc == 35 || imc <= 40){
+    return statusImc.textContent = "Obesidade grau II"
+  }else{
+    return statusImc.textContent = "Obesidade grau III"
+  }
+}
+
+inicio();
