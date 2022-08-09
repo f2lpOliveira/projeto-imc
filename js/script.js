00a7dabf5969b59fa44f2f5fd677f4b5e5b35caa
f2lpOliveira <devfullstack@disponivel.online>
2022-08-09 18:10:04 -0300

    Refatoração de código

--- js/script.js
+++ js/script.js
@@ -3,43 +3,52 @@
 * @author linkedin.com/in/f2lp-oliveira
 */
 
-let peso = document.querySelector('#input-peso');
-let altura = document.querySelector('#input-altura');
-let resultadoImc = document.querySelector('#resultado-imc');
 
-function inicio(){
-  let buttonCalculeImc = document.querySelector('#calcule-imc');
+function start(){
+  let buttonCalculeImc = document.querySelector('#calculate-imc');
   buttonCalculeImc.addEventListener('click', handleButtonClick);
 }
-function calculaImc(){
-  return peso / altura ** 2;
-}
+
 function handleButtonClick(){
-  peso = parseFloat(peso.value.replace(",","."));
-  altura = parseFloat(altura.value.replace(",","."));
-  imc = calculaImc();
-  resultadoImc.textContent = imc.toFixed(2).replace(".",",");
-  statusImc();
-}
+  let inputWeight = document.querySelector('#input-weight');
+  let inputHeight = document.querySelector('#input-height');
+  let imcResult = document.querySelector('#imc-result');
+  
+  let weight = Number(inputWeight.value);
+  let height = Number(inputHeight.value);
+  
+  let imc = calculateImc(weight, height);
+  let formattedImc = imc.toFixed(2).replace('.',',');
 
-function statusImc(){
-  let statusImc = document.querySelector('#status-imc');
+  imcResult.textContent = formattedImc;
+  
+  tabelaImc();
 
-  if(imc == 16 || imc <= 16.9){
-    return statusImc.textContent = "Muito abaixo do peso"
-  }else if(imc == 17 || imc <= 18.4){
-    return statusImc.textContent = "Abaixo do peso"
-  }else if(imc == 18.5 || imc <= 24.9){
-    return statusImc.textContent = "Peso normal"
-  }else if(imc == 25 || imc <= 29.9){
-    return statusImc.textContent = "Acima do peso"
-  }else if(imc == 30 || imc <= 34.9){
-    return statusImc.textContent = "Obesidade grau I"
-  }else if(imc == 35 || imc <= 40){
-    return statusImc.textContent = "Obesidade grau II"
-  }else{
-    return statusImc.textContent = "Obesidade grau III"
+    function tabelaImc(){
+      let imcStatus = document.querySelector('#imc-status');
+      
+      if(imc < 16){
+        return imcStatus.textContent = "Valor inválido";
+      }else if(imc == 16 || imc <= 16.9){
+        return imcStatus.textContent = "Muito abaixo do peso";
+      }else if(imc == 17 || imc <= 18.4){
+        return imcStatus.textContent = "Abaixo do peso";
+      }else if(imc == 18.5 || imc <= 24.9){
+        return imcStatus.textContent = "Peso normal";
+      }else if(imc == 25 || imc <= 29.9){
+        return imcStatus.textContent = "Acima do peso";
+      }else if(imc == 30 || imc <= 34.9){
+        return imcStatus.textContent = "Obesidade grau I";
+      }else if(imc == 35 || imc <= 40){
+        return imcStatus.textContent = "Obesidade grau II";
+      }else{
+        return imcStatus.textContent = "Obesidade grau III";
+      }
   }
 }
 
-inicio();
+function calculateImc(weight, height){
+  return weight / height ** 2;
+}
+
+start();
